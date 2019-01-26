01fc68b06e0d5d6a80ce1b1915720feac0b1c9de
bobo <418474125@qq.com>
2019-01-26 10:58:54 +0800

    City类添加

---
+++ client/GGJ/Assets/Script/City.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+
+public class City
+{
+    public string id;
+    public string name;
+    public List<Edge> adjs;
+
+    public City(string _id, string _name, string _adjs)
+    {
+        id = _id;
+        name = _name;
+        string[] endCityIds = _adjs.Split(':');
+        adjs = new List<Edge>();
+        for(int i = 0; i < endCityIds.Length; ++i)
+        {
+            adjs.Add(new Edge(endCityIds[i]));
+        }
+    }
+}
+
+public class Edge
+{
+    public string endCityId;
+    //public int price;
+
+    public Edge(string _endCityId)
+    {
+        endCityId = _endCityId;
+    }
+}
